53cd720d81ef1c84c539f6374ef198972d5b9989
ubuntu <user@ubuntu.(none)>
2013-11-03 21:54:44 -0800

    first

---
+++ pyter.py
@@ -0,0 +1,14 @@
+from __future__ import division
+import operators
+import math
+
+env = dict(vars(math)) + dict(vars(operators))
+
+def eval():
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
