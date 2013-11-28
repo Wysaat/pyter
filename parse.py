0030074b17644bae27392421e77a34d9c87563c5
Wysaat <Wysaat@gmail.com>
2013-11-28 13:46:59 +0000

    33th

---
+++ parse.py
@@ -0,0 +1,30 @@
+def parse_atoms():
+	pass
+
+	def parse_identifiers():
+		pass
+
+	def parse_literals():
+		pass
+
+	def parse_parenthesized_forms():
+		pass
+
+	def parse_list_displays():
+		pass
+
+	def parse_generator_displays():
+		pass
+
+	def parse_dictionary_displays():
+		pass
+
+	def parse_set_displays():
+		pass
+
+    # deprecated in Python3
+	def parse_string_conversions():
+		pass
+
+	def parse_yield_expressions():
+		pass
