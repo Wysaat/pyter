42b0035dfedd7fd15cea4a22e11f9d79461eded4
Wysaat <Wysaat@gmail.com>
2014-02-04 14:06:28 +0800

    92nd

--- cpyter.c
+++ cpyter.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct {
-    char string[1024];
-    int index;
-    int line_number;
-} global = {
-    .string = "hello, world",
-    .index = 0,
-    .line_number = 0,
-};
-
-int line_number;
-
-// "<>" is deprecated in Python 3
+/* "<>" is deprecated in Python 3 */
 char tokens[100][4] = { "+", "-", "*", "**", "/", "//", "%",
                         "<<", ">>", "&", "|", "^", "~",
                         "<", ">", "<=", ">=", "==", "!=", "<>",
@@ -27,40 +16,75 @@ char tokens[100][4] = { "+", "-", "*", "**", "/", "//", "%",
                         "+=", "-=", "*=", "/=", "//=", "%=", "**=",
                         ">>=", "<<=", "&=", "^=", "|=", " " };
 
-char *interactive_get_line() {
+struct item {
+    char content[1024];
+    struct item *next;
+};
+
+struct {
     char string[1024];
-    char *bitch = "hell";
-    printf(">>> ");
-    gets(string);
-    return string;
+    int index;
+    int line_number;
+    struct item items_head;
+} global = {
+    .string = "",
+    .index = 0,
+    .line_number = 0,
+    .items_head = { " ", 0 },
+};
+
+void add_item(char *word) {
+    struct item *ptr = &global.items_head;
+    struct item *new_item = (struct item *)malloc(sizeof(struct item));
+    strcpy(new_item->content, word);
+    new_item->next = 0;
+    while (ptr->next != 0)
+        ptr = ptr->next;
+    ptr->next = new_item;
 }
 
-void raw_read(char *string, int index) {
+void show_items() {
+    struct item *ptr = &global.items_head;
+    while (ptr->next != 0) {
+        puts(ptr->next->content);
+        ptr = ptr->next;
+    }
+}
+
+void *interactive_get_line() {
+    printf(">>> ");
+    gets(global.string);
+}
+
+void raw_read() {
     int i;
+    char *string = global.string;
+    int index = global.index;
+
     while (string[index] == ' ' || string[index] == '\t') {
-        index ++;
+        index++;
     }
     char item[1024];
     strncpy(item, string + index, 3);
+    item[3] = 0;
     for (i = 0; strcmp(tokens[i], " ") != 0; i++) {
-        if (strcmp(item, tokens[i]) == 0)
-            return 0;
+        if (strcmp(item, tokens[i]) == 0) {
+            add_item(item);
+            index += 3;
+            // global.index = index;
+            return;
+        }
     }
-    puts(item);
-    return 0;
 }
-
-char *string;
 
 int main()
 {
-    char *index = "1024";
-    puts(index);
-    puts(global.string);
     do {
-        string = interactive_get_line();
-        puts(string);
-    } while (strcmp(string, "exit") != 0);
+        interactive_get_line();
+        puts(global.string);
+        raw_read();
+        show_items();
+    } while (strcmp(global.string, "exit") != 0);
 
     return 0;
 }
